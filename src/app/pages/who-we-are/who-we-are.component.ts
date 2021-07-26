adcdf760a74d1e000d3aa2c1c8b57ff1a8b0c92d
andrebrandaodf <andrebrandaodf@gmail.com>
2021-07-26 15:58:08 -0300

    Adjusting page who-we-are

--- src/app/pages/who-we-are/who-we-are.component.ts
+++ src/app/pages/who-we-are/who-we-are.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-who-we-are',
@@ -7,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WhoWeAreComponent implements OnInit {
 
-  constructor() { }
+  innerWidth: number = 0;
 
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.innerWidth = window.innerWidth;
+  }
+
+  constructor() {
+    this.innerWidth = window.innerWidth;
+  }
   ngOnInit(): void {
   }
 
